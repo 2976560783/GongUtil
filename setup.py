4cea6fc1a8c641ca4943efb50525b86a192c4cbc
Administrator <2976560783@qq.com>
2019-07-28 08:52:38 +0800

    开发库

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='Base-gong',
+    version='1',
+    author='gong',
+    author_email='2976560783@qq.com',
+    description='A small example',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=''
+)
